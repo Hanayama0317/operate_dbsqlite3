6a846a09bc4ce6c787be4757c2fe42903113e656
Hanayama0317 <mogu0107hana@gmail.com>
2018-10-16 01:28:44 +0900

    first commit

--- operate_dbsqlite3.py
+++ operate_dbsqlite3.py
@@ -16,3 +16,24 @@ class accessDBsqlite3():
                 resultList.append(x)
             return resultList
 
+    def getAllTable(self):
+        with closing(sqlite3.connect(self.__dbname)) as conn:
+            c = conn.cursor()
+                
+            c.execute("select * from sqlite_master where type='table'")
+            resultList = []
+            for x in c.fetchall():
+                resultList.append(x)
+            return resultList
+        
+    def createRecode(self, table, recode):
+        with closing(sqlite3.connect(self.__dbname)) as conn:
+            c = conn.cursor()
+                
+            sql = u"insert into %s values %s" % (table,recode)
+            print(sql)
+            c.execute(sql)    
+            conn.commit()  
+            
+
+
